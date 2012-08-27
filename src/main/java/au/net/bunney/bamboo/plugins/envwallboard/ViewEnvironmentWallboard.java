ed9f7a90133396e674001b0618481de09fccd6ae
Gavin Bunney <gavin.bunney@gmail.com>
2012-08-27 14:46:42 +1000

    Added initial wallboard + cut for admin screens

---
+++ src/main/java/au/net/bunney/bamboo/plugins/envwallboard/ViewEnvironmentWallboard.java
@@ -0,0 +1,112 @@
+package au.net.bunney.bamboo.plugins.envwallboard;
+
+import com.atlassian.bamboo.ww2.BambooActionSupport;
+import org.apache.log4j.Logger;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.*;
+
+public class ViewEnvironmentWallboard extends BambooActionSupport {
+
+    private static final Logger log = Logger.getLogger(ViewEnvironmentWallboard.class);
+
+    private static final String THREAD_GROUP_NAME = "ViewEnvironmentWallboardThreadGroup";
+
+    private static final int DEFAULT_SECONDS_BEFORE_NEXT_REFRESH = 15;
+    private int secondsBeforeNextRefresh = DEFAULT_SECONDS_BEFORE_NEXT_REFRESH;
+
+    private List<HashMap<String, String>> environments = new ArrayList<HashMap<String, String>>();
+    private ThreadGroup threadGroup = new ThreadGroup(THREAD_GROUP_NAME);
+
+    @Override
+    public String doDefault() throws Exception {
+
+        for (final HashMap<String, String> environment : environments) {
+            final String threadName = environment.get("name");
+
+            Runnable runnableBlock = new Runnable() {
+                public void run() {
+                    connect(environment);
+                    log.debug(String.format("%s - Completed", threadName));
+                }
+            };
+
+            Thread childRunner = new Thread(threadGroup, runnableBlock, "");
+            log.debug(String.format("Start thread runner '%s'", threadName ));
+            childRunner.start();
+        }
+
+        // wait for all the child threads to finish
+        Thread[] childThreads = new Thread[threadGroup.activeCount()];
+        threadGroup.enumerate(childThreads);
+        for (Thread childThread : childThreads) {
+            childThread.join();
+        }
+
+        return SUCCESS;
+    }
+
+    protected void connect(HashMap<String, String> environment) {
+        try {
+            URL url = new URL(environment.get("url"));
+            HttpURLConnection yc = (HttpURLConnection) url.openConnection();
+
+            String usernamePassword = environment.get("auth");
+            if (usernamePassword != null) {
+                String encoded = new sun.misc.BASE64Encoder().encode(usernamePassword.getBytes());
+                yc.setRequestProperty("Authorization", "Basic " + encoded);
+            }
+
+            yc.setConnectTimeout(5 * 1000);
+            yc.setReadTimeout(10 * 1000);
+            yc.connect();
+            Scanner scanner = new Scanner(yc.getInputStream());
+
+            try {
+                HashMap<String, String> buildDetails = new HashMap<String, String>();
+
+                while ( scanner.hasNextLine() ){
+                    String line = scanner.nextLine();
+                    if (line.equals("[build]"))
+                        continue;
+
+                    Scanner lineScanner = new Scanner(line);
+                    lineScanner.useDelimiter("=");
+                    if ( lineScanner.hasNext() ){
+                        String name = lineScanner.next().trim();
+                        String value = lineScanner.next().trim();
+
+                        buildDetails.put(name, value);
+                    }
+                }
+
+                environment.put("status", "alive");
+                environment.put("buildNumber", buildDetails.get("buildNumber"));
+                environment.put("buildTimeStamp", buildDetails.get("buildTimeStamp"));
+                environment.put("buildRevision", buildDetails.get("buildRevision"));
+            } finally {
+                scanner.close();
+            }
+
+        } catch (Exception e) {
+            log.info("Environment wallboard exception: " + e);
+            environment.put("status", "dead");
+        }
+    }
+
+    public List<HashMap<String, String>> getEnvironments() {
+        return environments;
+    }
+
+    public int getSecondsBeforeNextRefresh() {
+        if(secondsBeforeNextRefresh < DEFAULT_SECONDS_BEFORE_NEXT_REFRESH) {
+            return DEFAULT_SECONDS_BEFORE_NEXT_REFRESH;
+        }
+        return secondsBeforeNextRefresh;
+    }
+
+    public void setSecondsBeforeNextRefresh(int secondsBeforeNextRefresh) {
+        this.secondsBeforeNextRefresh = secondsBeforeNextRefresh;
+    }
+}
---
+++ src/main/resources/viewEnvironmentWallboard.ftl
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>[@ww.text name='au.net.bunney.bamboo.plugins.envwallboard.title' /]</title>
+
+    <meta http-equiv="X-UA-Compatible" content="IE=EDGE" />
+
+    <meta http-equiv="Pragma" content="no-cache"/>
+    <meta http-equiv="Expires" content="-1"/>
+    <meta name="decorator" content="none">
+
+    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;"/>
+    <meta name="apple-mobile-web-app-capable" content="yes"/>
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
+
+    ${webResourceManager.requireResource("au.net.bunney.bamboo.plugins.bamboo-environment-wallboard:environment-wallboard-resources")}
+    ${webResourceManager.requiredResources}
+</head>
+<body>
+
+[#if environments.size() == 0]
+    <div class="no-builds">
+        <h3>No environments are configured for displaying</h3>
+        (this could be due to a filter or viewing permissions)
+    </div>
+
+[#else]
+    [#if environments.size() == 1]
+        [#assign boxWidth = "100%"]
+    [#elseif environments.size() == 2]
+        [#assign boxWidth = "50%"]
+    [#elseif environments.size() == 3]
+        [#assign boxWidth = "33%"]
+    [#else]
+        [#assign boxWidth = "25%"]
+    [/#if]
+
+    <script>
+      window.setTimeout(function(){window.location.href=window.location.href},${secondsBeforeNextRefresh}*1000);
+    </script>
+
+    [#list environments as environment]
+
+        [#if environment.status.equals('alive')]
+            <div class="environment" style="width: ${boxWidth};">
+                <div class="up">
+                    <div class="name">${environment.name}</div>
+                    <div class="build">${environment.buildNumber}</div>
+                    <div class="timeStamp">${environment.buildTimeStamp}</div>
+                    <div class="revision">${environment.buildRevision}</div>
+                </div>
+            </div>
+        [#else]
+            <div class="environment" style="width: ${boxWidth};">
+                <div class="down">
+                    <div class="name">${environment.name}</div>
+                    <div class="build">[@ww.text name='au.net.bunney.bamboo.plugins.envwallboard.status.dead'/]</div>
+                    <div class="details">&nbsp;</div>
+                </div>
+            </div>
+        [/#if]
+
+    [/#list]
+[/#if]
+</body>
+</html>
